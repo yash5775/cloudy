d041dca9ad8b90ab02d832e7b3ba22045fe9c0ee
yashkumar <yashchaniyara123@gmail.com>
2024-12-25 14:07:57 +0530

    install django,  Remove unused binary files and npmignore from node_modules

--- main/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from .models import ExampleModel
-
-def index(request):
-    examples = ExampleModel.objects.all()
-    return render(request, 'main/index.html', {'examples': examples})
